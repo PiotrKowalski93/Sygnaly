6f3561100c2343c7e060ed6dc16ca3b764589c52
Małgorzata Świtała <gswitala211@gmail.com>
2017-01-25 15:54:54 +0100

    czujnik odleglosci wykresy

--- Sygnaly/SymulatorCzujnikaOdleglosci.xaml.cs
+++ Sygnaly/SymulatorCzujnikaOdleglosci.xaml.cs
@@ -25,9 +25,10 @@ namespace Sygnaly
     {
         public List<KeyValuePair<double, double>> punkty;
         Sygnal A;
+        Sygnal przesuniety;
         int odleglosc = 100;
         int predkoscObiektu;
-        int predkochOsrodka;
+        int predkoscOsrodka;
         bool nadal=true;
         bool kierunek = true;
         public SymulatorCzujnikaOdleglosci()
@@ -63,21 +64,37 @@ namespace Sygnaly
 
             nadal = true;
             DateTime startTime = DateTime.Now;
+            czestotliwosc.IsEnabled = false;
+            int czestosc = int.Parse(czestotliwosc.Text.Trim());
             rzeczywistaPredkosc.IsEnabled = false;
             predkoscObiektu = int.Parse(rzeczywistaPredkosc.Text.Trim());
             predkoscRozchodzenia.IsEnabled = false;
-            predkochOsrodka = int.Parse(predkoscRozchodzenia.Text.Trim());
+            predkoscOsrodka = int.Parse(predkoscRozchodzenia.Text.Trim());
             while (nadal==true)
             {
+                DateTime posredni = DateTime.Now;
+                TimeSpan roznicaPosrednia = posredni - startTime;
+                double czasPrzemieszczeniaPosredniego = roznicaPosrednia.TotalMilliseconds;
+                int drogaPosrednia = (int)(predkoscObiektu * czasPrzemieszczeniaPosredniego/1000);
+                int odlegloscPosrednia=odleglosc;
+                if (kierunek == true) odlegloscPosrednia += drogaPosrednia;
+                else odlegloscPosrednia -= drogaPosrednia;
+                int czas=odlegloscPosrednia/(predkoscOsrodka/1000);
+
+                await Task.Delay(czas);
+                wyswietlSygnalPrzesuniety(A, czas);
+                Czas.Text = (czas).ToString();
                 DateTime stopTime = DateTime.Now;
                 TimeSpan roznica = stopTime - startTime;
                 startTime = stopTime;
-                double czasPrzemieszczenia = roznica.TotalSeconds;
+                int czasPrzemieszczenia = (int)(roznica.TotalSeconds);
                 int droga = (int)(predkoscObiektu * czasPrzemieszczenia);
                 if (kierunek == true) odleglosc += droga;
                 else odleglosc -= droga;
                 rzeczywistaOdleglosc.Text = odleglosc.ToString();
-                await Task.Delay(2000);
+
+                int czekaj = czestosc - (czasPrzemieszczenia*1000);
+                if (czekaj>0) await Task.Delay(czekaj);
             }
         }
         private void Stop_Click(object sender, RoutedEventArgs e)
@@ -88,6 +105,40 @@ namespace Sygnaly
         {
             if (kierunek == true) kierunek = false;
             else kierunek = true;
+        }
+        private void wyswietlSygnalPrzesuniety(Sygnal A, int czas)
+        {
+            Przesuniety.Series.Clear();
+            przesuniety = new Sygnal();
+            przesuniety.x = new List<System.Numerics.Complex>();
+            przesuniety.y = new List<System.Numerics.Complex>();
+
+            for (int i = 0; i < A.x.Count; i++)
+            {
+                if (A.x[i].Real- (czas / 1000.0) > 0) przesuniety.x.Add(A.x[i]-(double)(czas/1000.0));
+                else przesuniety.x.Add(A.x[i] + 10 - (czas / 1000.0));
+                przesuniety.y.Add(A.y[i]);
+            }
+
+            LineSeries mySeries = new LineSeries();
+            mySeries.Title = "Sygnał odebrany";
+            mySeries.IndependentValueBinding = new Binding("Key");
+            mySeries.DependentValueBinding = new Binding("Value");
+
+            Style style = new Style(typeof(LineDataPoint));
+            style.Setters.Add(new Setter(LineDataPoint.TemplateProperty, null));
+            mySeries.DataPointStyle = style;
+
+            punkty = new List<KeyValuePair<double, double>>();
+
+            for (int i = 0; i < przesuniety.x.Count; i++)
+            {
+                punkty.Add(new KeyValuePair<double, double>(przesuniety.x[i].Real, przesuniety.y[i].Real));
+            }
+
+            mySeries.ItemsSource = punkty;
+
+            Przesuniety.Series.Add(mySeries);
         }
     }
    
